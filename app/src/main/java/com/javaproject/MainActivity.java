db784bddbe6a32157f2bff56af1610f40c9f61b8
Reagent <reagentoj@gmail.com>
2020-10-29 16:13:08 +0300

    Initial commit

---
+++ app/src/main/java/com/javaproject/MainActivity.java
@@ -0,0 +1,28 @@
+package com.javaproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this,
+    DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.textBox);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+
+
+    }
+}
